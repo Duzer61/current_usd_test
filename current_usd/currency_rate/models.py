ed0703b14b7c5d4fea7c8cc8c20cdd36d33c8a1b
Duzer61 <dmi194@yandex.ru>
2024-01-23 17:46:41 +0300

    Добавил модель курсов

--- current_usd/currency_rate/models.py
+++ current_usd/currency_rate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrencyRate(models.Model):
+    """
+    Модель курса валют.
+    """
+
+    currency = models.CharField(verbose_name='Код валюты', max_length=3)
+    rate = models.FloatField(verbose_name='Курс')
+    date = models.DateTimeField(verbose_name='Дата', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Курс валюты'
+        verbose_name_plural = 'Курсы валют'
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'{self.currency}: {self.rate}'
